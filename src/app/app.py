8f204d79e2385391a27b36ba1ff29bae933a9758
Levi Pare <levipare@gmail.com>
2026-04-21 13:00:26 -0400

    remove unused title

--- src/app/app.py
+++ src/app/app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-# st.title("CS3540 Final Project")
-
 pages = [
     st.Page("pages/pca.py", title="PCA", icon=":material/scatter_plot:"),
     st.Page("pages/models.py", title="Models", icon=":material/model_training:"),
